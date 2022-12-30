4b2a7e1ea0c6f5b5050eb0e255e99fdd4d12b168
Arend <matheus.arend@univille.br>
2022-12-30 18:23:58 -0300

    step3

--- view/sendFiles.php
+++ view/sendFiles.php
@@ -10,32 +10,39 @@
     $extensao;
     $pasta;
     $hashName;
+    $hashNameWithExt;
     $isAllCorrect;
+    $tamanhoArquivo;
+    $path;
+    $progressStatus;
 
-
+    //Se existe arquivo no input.
     if(isset($_FILES['arquivo'])){
 
         $arquivo = $_FILES['arquivo'];
 
+        //Se existe erro no envio do arquivo. (Logic 1)
         if($arquivo['error']){
 
-            die("Erro ao carregar o arquivo.");
+            die("Logic 1: Erro ao carregar o arquivo.");
 
         }
 
-
+        //Se o tamanhdo do arquivo for maior que 2mb. (Logic 2)
         if($arquivo['size'] > 2097152){  //2mb
 
-            die("Arquivo muito grande. Tamanho máximo permitido: 2Mg.");
+            die("Logic 2: Erro! Arquivo muito grande. Tamanho máximo permitido: 2Mg.");
 
         }else{
 
+            $tamanhoArquivo = $arquivo['size'];
             $nomeDoArquivo = $arquivo['name'];
             $extensao = strtolower(pathinfo($nomeDoArquivo, PATHINFO_EXTENSION));
 
+            //Se o arquivo não conter extensão .jpg ou .png. (Logic 3)
             if($extensao != "jpg" && $extensao != "png"){
 
-                die("O tipo do arquivo enviado deve estar em formato .jpg ou .png");
+                die("Logic 3: Erro! O tipo do arquivo enviado deve estar em formato .jpg ou .png");
 
             }else{
 
@@ -43,16 +50,29 @@
                 echo $hashName = uniqid();
 
                 $hashNameWithExt = $hashName . "." . $extensao;
+                $path = $pasta . $hashName . "." . $extensao;
 
-                $isAllCorrect = move_uploaded_file($arquivo['tmp_name'], $pasta . $hashName . "." . $extensao);
+                $isAllCorrect = move_uploaded_file($arquivo['tmp_name'], $path);
 
+                //Se a deslocação do arquivo para a pasta destino foi realizado com sucesso. (Logic 4)
                 if($isAllCorrect){
 
-                    echo "<p>Arquivo enviado com sucesso! Para acessá-lo, clique aqui: <a target='_blank' href='files/$hashNameWithExt'> Clique aqui </a> </p>";
+                    //Se o tamanho da string arquivo for maior que 100 caracteres. (Logic 5)
+                    if(strlen($nomeDoArquivo) > 100){
+
+                        die("Logic 5: O tamanho do nome do arquivo é grande demais para ser inserido no banco de dados");
+
+                    }
+
+                    $progressStatus = 0;
+
+                    $mysqli->query("INSERT INTO arquivos(pathDir, originalFileName, fileSize, progressStatus) VALUES('$path', '$nomeDoArquivo', $tamanhoArquivo, $progressStatus)") or die($mysqli->error);
+
+                    echo "<p>Arquivo enviado com sucesso!</p>";
 
                 }else{
 
-                    echo "Falha ao enviar o arquivo";
+                    die("Logic 4: Falha ao enviar o arquivo");
 
                     // SALVAR NO LOG MESMO QUE TENHA DADO ERRO
 
@@ -62,11 +82,13 @@
 
         }
 
-        //var_dump($arquivo);
+        var_dump($arquivo);
 
     }
 
-    
+$sql_query = $mysqli->query("SELECT * FROM arquivos") or die($mysqli->error);
+
+
 
     
 ?>
@@ -104,6 +126,45 @@
         </form>    
 
     </div>
+
+    <br><br><br><br>
+
+    <h4>Lista de arquivos</h4>
+    <table border="1" cellpadding="10" style="width: 100%;">
+        <thead>
+
+            <th>Preview</th>
+            <th>Arquivo</th>
+            <th>Data de envio</th>
+            <th>Status</th>
+
+        </thead>
+    
+        <tbody style="width: 100%;">
+
+            <?php
+
+                while($arquivo = $sql_query->fetch_assoc()){
+                    
+            ?>
+                    <tr>
+
+                        <td> <img height='50' src='<?php $arquivo['pathDir']; ?>' alt=''> </td>
+                        <td> <?php echo $arquivo['pathDir']; ?> </td>
+                        <td> <?php echo date("d/m/Y H:i", strtotime($arquivo['uploadDate'])); ?> </td>
+                        <td> <?php echo $arquivo['progressStatus']; ?> </td>
+
+                    </tr>
+            
+            <?php
+            
+                }
+
+            ?>
+
+        </tbody>
+
+    </table>
 
 </body>
 
